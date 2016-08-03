3a0649b0572f72aff1e6f8755974ca915bb608af
景天 <jingtian@naitang.com>
2016-08-03 15:39:58 +0800

    polyfill

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var router = express.Router()
+var router = express.Router() // eslint-disable-line
 var multipart = require('connect-multiparty')
 var multipartMiddleware = multipart()
 
