1c441924b3691458687ea622e21aae636d5d30c3
lincenying <lincenying@qq.com>
2016-08-06 21:22:10 +0800

    comment.jsx 改成 es6 class写法

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import * as articleActions from '../actions/article'
@@ -6,58 +6,45 @@ import * as globalsActions from '../actions/globals'
 import {CommentItem} from "./comment-item"
 import api from '../api'
 
-const comment = React.createClass({
-    propTypes: {
-        fetchComment: PropTypes.func,
-        postComment: PropTypes.func,
-        comment: PropTypes.object
-    },
-    getInitialState: function() {
-        return {
+class comment extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
             username: '',
             content: ''
-        };
-    },
-    componentDidMount() {
-        this._fetchComment()
-    },
+        }
+        this.handlePostComment = this.handlePostComment.bind(this)
+        this.handleLoadMore = this.handleLoadMore.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
     _fetchComment(page = 1) {
         const {fetchComment, id} = this.props
-        fetchComment({
-            action: 'comment',
+        fetchComment({action: 'comment', id, page})
+    }
+    async handlePostComment() {
+        const {postComment, setMessage, id} = this.props
+        const {username, content} = this.state
+        if (this.state.content === '') {
+            setMessage({type: 'error', content: '请输入评论内容!'})
+            return false
+        }
+        const json = await api.getData({
+            action: 'postComment',
             id,
-            page
+            content,
+            username: username || '匿名用户'
         })
-    },
-    handlePostComment() {
-        (async () => {
-            const {postComment, setMessage, id} = this.props
-            const {username, content} = this.state
-            if (this.state.content === '') {
-                setMessage({
-                    type: 'error',
-                    content: '请输入评论内容!'
-                })
-                return false
-            }
-            const json = await api.getData({
-                action: 'postComment',
-                id,
-                content,
-                username: username || '匿名用户'
-            })
-            postComment([json.data])
-            this.setState({username: '', content: ''})
-            setMessage('发表评论成功!')
-        })()
-    },
+        postComment([json.data])
+        this.setState({username: '', content: ''})
+        setMessage('发表评论成功!')
+    }
     handleLoadMore() {
         const {page} = this.props.comment
         this._fetchComment(page + 1)
-    },
+    }
     handleChange(type, event) {
         this.setState({[type]: event.target.value})
-    },
+    }
     render() {
         const {list, hasNext} = this.props.comment
         const lists = list.map((list, index) => {
@@ -65,11 +52,15 @@ const comment = React.createClass({
                 <CommentItem key={list._id} list={list}></CommentItem>
             )
         })
-        const more = hasNext ? (
-            <div className="bcmtmore s-bd2">
-                <div className="bcmtlsta"><a onClick={this.handleLoadMore} href="javascript:;" className="s-fc2 ztag">查看更多</a></div>
-            </div>
-        ) : ''
+        const more = hasNext
+            ? (
+                <div className="bcmtmore s-bd2">
+                    <div className="bcmtlsta">
+                        <a onClick={this.handleLoadMore} href="javascript:;" className="s-fc2 ztag">查看更多</a>
+                    </div>
+                </div>
+            )
+            : ''
         return (
             <div className="box">
                 <div className="comment">
@@ -77,7 +68,7 @@ const comment = React.createClass({
                     <div className="bcmt">
                         <div className="s-fc0 ztag ztag_tips">由于该用户的权限设置，您暂时无法进行评论...</div>
                         <div className="bcmtadd">
-                            <input value={this.state.username} onChange={this.handleChange.bind(this, 'username')} type="text" className="form-control" placeholder="请输入昵称" />
+                            <input value={this.state.username} onChange={this.handleChange.bind(this, 'username')} type="text" className="form-control" placeholder="请输入昵称"/>
                             <textarea value={this.state.content} onChange={this.handleChange.bind(this, 'content')} id="content" className="form-control" placeholder="请输入评论内容"></textarea>
                             <div className="bcmtbtn">
                                 <span className="ztag ztag_tips">提示</span>
@@ -87,7 +78,7 @@ const comment = React.createClass({
                         </div>
                         <div className="bcmtlst">
                             <ul className="clearfix ztag">
-                                { lists }
+                                {lists}
                             </ul>
                         </div>
                         {more}
@@ -96,12 +87,15 @@ const comment = React.createClass({
             </div>
         )
     }
-})
+}
 
+comment.propTypes = {
+    fetchComment: PropTypes.func,
+    postComment: PropTypes.func,
+    comment: PropTypes.object
+}
 function mapStateToProps(state) {
-    return {
-        comment: state.article.comment
-    }
+    return {comment: state.article.comment}
 }
 
 function mapDispatchToProps(dispatch) {
